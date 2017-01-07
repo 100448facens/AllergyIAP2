a7122d54e9ccbabe52ba9395f1ec2321e40e896e
jose <100448facens@gmail.com>
2017-01-07 18:22:02 +0100

    Create Structure base for anothers activities

--- app/src/main/java/com/allergyiap/activities/LevelAllergyFragment.java
+++ app/src/main/java/com/allergyiap/activities/LevelAllergyFragment.java
@@ -2,6 +2,7 @@ package com.allergyiap.activities;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -15,8 +16,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.allergyiap.R;
-import com.allergyiap.adapters.AllergiesAdapter;
-import com.allergyiap.entities.AllergyLevelEntity;
+import com.allergyiap.adapters.AllergiesLevelAdapter;
+import com.allergyiap.beans.Allergy;
+import com.allergyiap.beans.AllergyLevel;
+import com.allergyiap.service.AllergyLevelService;
+import com.allergyiap.utils.C;
 
 import java.util.List;
 
@@ -29,11 +33,11 @@ public class LevelAllergyFragment extends Fragment {
 
     MainActivity activity;
     Context context;
-    private AllergiesAdapter adapter;
+    private AllergiesLevelAdapter adapter;
     private RecyclerView recyclerView;
-    private AsyncTask<Void, Void, Void> task;
+    private AsyncTask<Void, Void, List<AllergyLevel>> task;
 
-    List<AllergyLevelEntity> allergy;
+    List<AllergyLevel> allergiesLevel;
 
     public LevelAllergyFragment() {
         // Required empty public constructor
@@ -83,38 +87,39 @@ public class LevelAllergyFragment extends Fragment {
     }
 
     private void loadData() {
-        task = new LoadAllergiesBT();
+        task = new LoadAllergyLevelBT();
         task.execute();
     }
 
-    private void loadAdapter(final List<AllergyLevelEntity> list) {
+    private void loadAdapter(final List<AllergyLevel> list) {
         Log.d(TAG, ".loadAdapter");
 
         if (adapter == null)
-            adapter = new AllergiesAdapter(context, list);
+            adapter = new AllergiesLevelAdapter(context, list);
         else
             adapter.setAllergies(list);
 
         recyclerView.setAdapter(adapter);
-        adapter.setOnItemClickListener(new AllergiesAdapter.OnItemClickListener() {
+        adapter.setOnItemClickListener(new AllergiesLevelAdapter.OnItemClickListener() {
 
             @Override
-            public void onItemClick(View view, int position, AllergyLevelEntity alertEntity) {
-                showAllergyDetail(alertEntity);
+            public void onItemClick(View view, int position, AllergyLevel allergyLevel, Allergy allergy) {
+                showAllergyDetail(allergy, allergyLevel);
             }
         });
     }
 
-    private void showAllergyDetail(AllergyLevelEntity alertEntity) {
-        /*Intent intent = new Intent(this, MapAllergyLevelsDetailsActivity.class);
+    private void showAllergyDetail(Allergy allergy, AllergyLevel allergyLevel) {
+        Intent intent = new Intent(activity, AllergyActivity.class);
         Bundle b = new Bundle();
-        b.putSerializable(C.IntentExtra.Sender.VAR_ALLERGY2, alertEntity);
+        b.putSerializable(C.IntentExtra.Sender.VAR_ALLERGY, allergy);
+        b.putSerializable(C.IntentExtra.Sender.VAR_ALLERGY2, allergyLevel   );
         intent.putExtras(b);
-        startActivity(intent);*/
+        startActivity(intent);
     }
 
 
-    private class LoadAllergiesBT extends AsyncTask<Void, Void, Void> {
+    private class LoadAllergyLevelBT extends AsyncTask<Void, Void, List<AllergyLevel>> {
 
         @Override
         protected void onPreExecute() {
@@ -123,15 +128,17 @@ public class LevelAllergyFragment extends Fragment {
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
-            return null;
+        protected List<AllergyLevel> doInBackground(Void... params) {
+
+            return AllergyLevelService.getAll();
         }
 
         @Override
-        protected void onPostExecute(Void result) {
+        protected void onPostExecute(List<AllergyLevel> result) {
             super.onPostExecute(result);
             activity.findViewById(R.id.progress_bar).setVisibility(View.GONE);
-            loadAdapter(allergy);
+            allergiesLevel = result;
+            loadAdapter(allergiesLevel);
         }
     }
 }
