856514702de42e36843949856b9840515a1b3621
jose <100448facens@gmail.com>
2017-01-10 14:19:43 +0100

    --

--- app/src/main/java/com/allergyiap/utils/ReceptorBoot.java
+++ app/src/main/java/com/allergyiap/utils/ReceptorBoot.java
@@ -10,6 +10,7 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 /**
  * This receiver start the alarm configuration because when turn off the mobile this service is closed
@@ -29,10 +30,14 @@ public class ReceptorBoot extends BroadcastReceiver {
         AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Calendar cal = Calendar.getInstance();
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        String hour = String.valueOf(preferences.getLong("time_alarm", 0));
+        long millis = preferences.getLong("time_alarm", 0);
+        String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
+                TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
 
-        cal.set(Calendar.HOUR_OF_DAY, TimePreference.getHour(hour));
-        cal.set(Calendar.MINUTE, TimePreference.getMinute(hour));
+
+        cal.set(Calendar.HOUR_OF_DAY, TimePreference.getHour(hms));
+        cal.set(Calendar.MINUTE, TimePreference.getMinute(hms));
         cal.set(Calendar.SECOND, 0);
         cal.set(Calendar.MILLISECOND, 0);
 
